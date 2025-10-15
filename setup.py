6988f1fc3ef25f75cc03d52fcf31c3446b09a9a8
AmerKone <a.kaouane@esi-sba.dz>
2025-10-15 11:08:41 +0100

    Chat is ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gym_chatbot",
+    version="0.1.0",
+    author="Ameur KAOUANE",
+    author_email="kaouaneamer26@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helpers.py
+touch src/main.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+echo 'Everything is set up!' 
