28b80ccfe82d3378f5ce0ec5e1be4ce11800c827
AmerKone <a.kaouane@esi-sba.dz>
2025-10-18 14:22:10 +0100

    Application is ready

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ app = Flask(__name__)
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = ''
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 index_name = "gym-chatbot"
 
--- src/prompt.py
+++ src/prompt.py
@@ -1,5 +1,5 @@
 system_prompt = ( "You are a helpful AI assistant, expert in gym and body building."
                     "Use the following pieces of context to answer the question at the end."
-                    " If you don't know the answer, just say that you don't know, don't try to make up an answer."
+                    " If you don't know the answer, just say that you only answer questions related to gym, fitness, and body building, don't try to make up an answer."
                     "\n\n"
                     "{context}")
